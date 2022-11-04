dea86d45e7172c707fa7103db3edf71d13656752
D-wade99 <105051747+D-wade99@users.noreply.github.com>
2022-11-04 08:06:11 +0000

    feat: Add Routes & Route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Profile from "./Profile";
 import Contact from './Contact'
+import {Routes, Route} from 'react-router-dom'
 
 
 function App() {
   return (
     <div>
-      <Profile />
-      <Contact />
+      <Routes>
+        <Route  path="/profile" element={<Profile/>} />
+        <Route  path="/contact" element={<Contact/>} />
+      </Routes>
     </div>
   );
 }
