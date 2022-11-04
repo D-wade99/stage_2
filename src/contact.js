08642fba3d475cd7d535eea70928e6698ac5544b
D-wade99 <105051747+D-wade99@users.noreply.github.com>
2022-11-04 08:08:47 +0000

    feat:  Update form element

--- src/contact.js
+++ src/contact.js
@@ -4,7 +4,14 @@ import "./App.css";
 
 function Contact() {
   return (
-    <>hello world!</>
+    <div className="container">
+      <form>
+        <input id="first_name" placeholder="First name"/>
+        <input id="last_name" placeholder="Last name"/>
+        <input id="message" />
+        <button id="btn_submit">submit</button>
+      </form>
+    </div>
   );
 }
 
